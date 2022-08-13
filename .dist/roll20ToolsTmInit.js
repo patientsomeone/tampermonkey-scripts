b28cf30f60c5de3175b3027b560e697891d28bc4
Joshua Robinson <grizzgrizz87@gmail.com>
2022-08-12 17:40:07 -0700

    Migrated to require

---
+++ .dist/roll20ToolsTmInit.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Roll 20 Player Tools
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://app.roll20.net/editor/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=roll20.net
+// @require      https://gitlab.com/robinsolutions/tampermonkey-scripts/-/raw/master/.dist/roll20Tools.js
+// @grant        none
+// ==/UserScript==
