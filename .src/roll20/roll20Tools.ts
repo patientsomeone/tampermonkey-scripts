1af447f3fc7807262882c0c5867b9ab2d5a0a10c
Joshua Robinson <grizzgrizz87@gmail.com>
2022-08-13 16:33:52 -0700

    Testing TM

--- .src/roll20/roll20Tools.ts
+++ .src/roll20/roll20Tools.ts
@@ -1,34 +1,34 @@
-import {AsyncUtil} from "../utilities/asyncUtil";
-import {log, logLine} from "../utilities/log";
-import {} from "../utilities/debugLog";
+// import {AsyncUtil} from "../utilities/asyncUtil";
+// import {log, logLine} from "../utilities/log";
+// import {} from "../utilities/debugLog";
 
-/* Import the Campaign type */
-import {campaign} from "./.r20Types.d";
-import {each} from "jquery";
-/* Declare Campaign as Global Constant */
-declare const Campaign: campaign;
+// /* Import the Campaign type */
+// import {campaign} from "./.r20Types.d";
+// import {each} from "jquery";
+// /* Declare Campaign as Global Constant */
+// declare const Campaign: campaign;
 
-const tmRoll20 = {
-    fetchPlayers: () => {
-        console.log("I will Fetch Players");
-    },
-    roundCounter: () => {
-        const counterWindow = "";
-        console.log("I will count rounds");
-    },
-    allCharacters: () => {
-        const allCharacters = Campaign.characters._byId;
+// const tmRoll20 = {
+//     fetchPlayers: () => {
+//         console.log("I will Fetch Players");
+//     },
+//     roundCounter: () => {
+//         const counterWindow = "";
+//         console.log("I will count rounds");
+//     },
+//     allCharacters: () => {
+//         const allCharacters = Campaign.characters._byId;
 
-        const eachCharacter = (individualItem, key, triggerNext, triggerCounter) => {
-            const currentCharacter = individualItem as campaign["characters"]["_byId"][0];
+//         const eachCharacter = (individualItem, key, triggerNext, triggerCounter) => {
+//             const currentCharacter = individualItem as campaign["characters"]["_byId"][0];
 
-            return console.log(currentCharacter.attributes.name);
-        };
+//             return console.log(currentCharacter.attributes.name);
+//         };
 
-        return AsyncUtil.eachOfSeries(allCharacters, eachCharacter);
-    }
-};
+//         return AsyncUtil.eachOfSeries(allCharacters, eachCharacter);
+//     }
+// };
 
 console.log("Running Tampermonkey");
 
-tmRoll20.allCharacters();
+// tmRoll20.allCharacters();
